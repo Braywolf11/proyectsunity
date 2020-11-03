e32e92320f3516c4925059acbe8b95db733b4ac1
braywolf11 <brayfer1893@gmail.com>
2020-11-02 19:15:54 -0500

    entrega

---
+++ Flappy-Bird-v1/Assets/Scripts/ObstaclesGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using Random = UnityEngine.Random;
+using UnityEngine;
+
+public class ObstaclesGenerator : MonoBehaviour
+{
+    public float tMax = 1;
+    public float tStart = 0;
+    public GameObject obstacule;
+    public float obsHeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject newObstacule = Instantiate(obstacule);
+        newObstacule.transform.position = transform.position + new Vector3 (0, 0, 0);
+        Destroy(gameObject, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(tStart > tMax)
+        {
+            GameObject newObstacule = Instantiate(obstacule);
+            newObstacule.transform.position = transform.position + new Vector3 (0, Random.Range(-obsHeight,obsHeight), 0);
+            Destroy(gameObject, 10);
+            tStart = 0;
+
+        }
+        else
+        {
+            tStart += Time.deltaTime;
+        }
+    }
+}
---
+++ Flappy-Bird-v1/Assets/Scripts/eceneController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject LoserCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void YouLose()
+    {
+        LoserCanvas.SetActive(true);
+        //SceneManager.loadScene(1);
+        Time.timeScale = 0;
+        
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene (0);
+    }
+}
